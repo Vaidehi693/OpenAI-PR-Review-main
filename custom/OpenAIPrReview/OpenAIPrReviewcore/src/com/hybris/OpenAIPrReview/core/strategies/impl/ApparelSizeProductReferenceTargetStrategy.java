1600d8b377c2521f9315fcbddd130ba65b01e5b7
VaidehiDastapure <vaidehi.dastapure@ibm.com>
2025-10-15 17:16:35 -0500

    hanges

--- custom/OpenAIPrReview/OpenAIPrReviewcore/src/com/hybris/OpenAIPrReview/core/strategies/impl/ApparelSizeProductReferenceTargetStrategy.java
+++ custom/OpenAIPrReview/OpenAIPrReviewcore/src/com/hybris/OpenAIPrReview/core/strategies/impl/ApparelSizeProductReferenceTargetStrategy.java
@@ -124,6 +124,12 @@ public class ApparelSizeProductReferenceTargetStrategy implements ProductReferen
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
+            System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
+            System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
+            System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
+            
+                        System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
+            System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
 
         }
         return result;
