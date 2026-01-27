082d0693c4e7a0fcd63e3d5b6fdf7ee2ddbbf217
VaidehiDastapure <vaidehi.dastapure@ibm.com>
2026-01-26 21:20:56 -0600

    PR review changes

--- custom/OpenAIPrReview/OpenAIPrReviewcore/src/com/hybris/OpenAIPrReview/core/strategies/impl/ApparelSizeProductReferenceTargetStrategy.java
+++ custom/OpenAIPrReview/OpenAIPrReviewcore/src/com/hybris/OpenAIPrReview/core/strategies/impl/ApparelSizeProductReferenceTargetStrategy.java
@@ -126,8 +126,6 @@ public class ApparelSizeProductReferenceTargetStrategy implements ProductReferen
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
             System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
-            System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
-            System.out.println("Intermediate Result: " + result);  // Unnecessary print statement
 
         }
         return result;
